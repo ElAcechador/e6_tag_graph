2240cc5eb41c2e82cb18adc5110b389654271969
ElAcechador <elacechador@live.ca>
2018-11-20 00:52:19 -0500

    Holy shit, it actually works

--- e6_tag_graph.py
+++ e6_tag_graph.py
@@ -86,29 +86,30 @@ def fetch_tag_data(tag_id=None, tagname=None):
     return tag_data
 
 
-tags_to_explore = ['mammal']
-
-current_tag = tags_to_explore.pop()
-
-implication_data = [{'id': 12831, 'consequent_id': 12054, 'predicate_id': 633522, 'pending': True, 'name': 'afrosoricid'}, {'id': 10128, 'consequent_id': 12054, 'predicate_id': 486038, 'pending': True, 'name': 'elephantidae'}, {'id': 10572, 'consequent_id': 12054, 'predicate_id': 14751, 'pending': True, 'name': 'shrew'}, {'id': 5036, 'consequent_id': 12054, 'predicate_id': 6103, 'pending': False, 'name': 'aardvark'}, {'id': 5035, 'consequent_id': 12054, 'predicate_id': 11504, 'pending': False, 'name': 'anteater'}, {'id': 3370, 'consequent_id': 12054, 'predicate_id': 3391, 'pending': False, 'name': 'antelope'}, {'id': 3307, 'consequent_id': 12054, 'predicate_id': 3487, 'pending': False, 'name': 'armadillo'}, {'id': 2808, 'consequent_id': 12054, 'predicate_id': 830, 'pending': False, 'name': 'bat'}, {'id': 2899, 'consequent_id': 12054, 'predicate_id': 265, 'pending': False, 'name': 'bear'}, {'id': 2738, 'consequent_id': 12054, 'predicate_id': 18297, 'pending': False, 'name': 'bovine'}, {'id': 4346, 'consequent_id': 12054, 'predicate_id': 39306, 'pending': False, 'name': 'camelid'}, {'id': 2645, 'consequent_id': 12054, 'predicate_id': 1068, 'pending': False, 'name': 'canine'}, {'id': 2904, 'consequent_id': 12054, 'predicate_id': 64042, 'pending': False, 'name': 'caprine'}, {'id': 2830, 'consequent_id': 12054, 'predicate_id': 3695, 'pending': False, 'name': 'cervine'}, {'id': 2860, 'consequent_id': 12054, 'predicate_id': 20050, 'pending': False, 'name': 'cetacean'}, {'id': 3189, 'consequent_id': 12054, 'predicate_id': 1258, 'pending': False, 'name': 'elephant'}, {'id': 2648, 'consequent_id': 12054, 'predicate_id': 3085, 'pending': False, 'name': 'equine'}, {'id': 12572, 'consequent_id': 12054, 'predicate_id': 618952, 'pending': False, 'name': 'euplerid'}, {'id': 2647, 'consequent_id': 12054, 'predicate_id': 180, 'pending': False, 'name': 'feline'}, {'id': 3227, 'consequent_id': 12054, 'predicate_id': 462, 'pending': False, 'name': 'giraffe'}, {'id': 12530, 'consequent_id': 12054, 'predicate_id': 503284, 'pending': False, 'name': 'giraffid'}, {'id': 7113, 'consequent_id': 12054, 'predicate_id': 9109, 'pending': False, 'name': 'gnoll'}, {'id': 2901, 'consequent_id': 12054, 'predicate_id': 1985, 'pending': False, 'name': 'hedgehog'}, {'id': 12058, 'consequent_id': 12054, 'predicate_id': 181786, 'pending': False, 'name': 'herpestid'}, {'id': 4595, 'consequent_id': 12054, 'predicate_id': 140257, 'pending': False, 'name': 'hippopotamus'}, {'id': 2898, 'consequent_id': 12054, 'predicate_id': 411, 'pending': False, 'name': 'human'}, {'id': 2905, 'consequent_id': 12054, 'predicate_id': 490, 'pending': False, 'name': 'hyena'}, {'id': 2646, 'consequent_id': 12054, 'predicate_id': 17459, 'pending': False, 'name': 'lagomorph'}, {'id': 5032, 'consequent_id': 12054, 'predicate_id': 114540, 'pending': False, 'name': 'manatee'}, {'id': 2903, 'consequent_id': 12054, 'predicate_id': 3112, 'pending': False, 'name': 'marsupial'}, {'id': 12520, 'consequent_id': 12054, 'predicate_id': 618306, 'pending': False, 'name': 'mephitid'}, {'id': 4010, 'consequent_id': 12054, 'predicate_id': 9859, 'pending': False, 'name': 'mole'}, {'id': 4349, 'consequent_id': 12054, 'predicate_id': 5557, 'pending': False, 'name': 'monotreme'}, {'id': 2900, 'consequent_id': 12054, 'predicate_id': 4993, 'pending': False, 'name': 'mustelid'}, {'id': 4966, 'consequent_id': 12054, 'predicate_id': 162470, 'pending': False, 'name': 'nimbat'}, {'id': 4515, 'consequent_id': 12054, 'predicate_id': 11180, 'pending': False, 'name': 'okapi'}, {'id': 5034, 'consequent_id': 12054, 'predicate_id': 316, 'pending': False, 'name': 'pangolin'}, {'id': 7529, 'consequent_id': 12054, 'predicate_id': 167452, 'pending': False, 'name': 'pinniped'}, {'id': 2907, 'consequent_id': 12054, 'predicate_id': 64397, 'pending': False, 'name': 'porcine'}, {'id': 2906, 'consequent_id': 12054, 'predicate_id': 21794, 'pending': False, 'name': 'primate'}, {'id': 12529, 'consequent_id': 12054, 'predicate_id': 602261, 'pending': False, 'name': 'proboscidean'}, {'id': 12067, 'consequent_id': 12054, 'predicate_id': 167951, 'pending': False, 'name': 'procyonid'}, {'id': 6367, 'consequent_id': 12054, 'predicate_id': 65815, 'pending': False, 'name': 'pronghorn'}, {'id': 2908, 'consequent_id': 12054, 'predicate_id': 919, 'pending': False, 'name': 'red_panda'}, {'id': 3206, 'consequent_id': 12054, 'predicate_id': 66300, 'pending': False, 'name': 'rhinoceros'}, {'id': 2818, 'consequent_id': 12054, 'predicate_id': 3661, 'pending': False, 'name': 'rodent'}, {'id': 4960, 'consequent_id': 12054, 'predicate_id': 4668, 'pending': False, 'name': 'sloth'}, {'id': 4225, 'consequent_id': 12054, 'predicate_id': 3178, 'pending': False, 'name': 'tanuki'}, {'id': 4670, 'consequent_id': 12054, 'predicate_id': 4127, 'pending': False, 'name': 'tapir'}, {'id': 5037, 'consequent_id': 12054, 'predicate_id': 153348, 'pending': False, 'name': 'viverrid'}]
+tags_to_explore = ['male']
 
 g = Digraph('G', filename='cluster.gv')
 
-for tag in implication_data:
-    tags_to_explore.append(tag['name'])
+while(len(tags_to_explore) != 0):
+    current_tag = tags_to_explore.pop()
 
-    alias_data = get_aliases_for_tag(tag['name'])
+    implication_data = get_implications_for_tag(current_tag)
 
-    with g.subgraph(name='cluster_{}'.format(tag['name'])) as c:
-        c.attr(style='filled')
+    for tag in implication_data:
+        tags_to_explore.append(tag['name'])
 
-        c.node_attr.update(style='filled', color='lightgrey')
+        alias_data = get_aliases_for_tag(tag['name'])
 
-        c.node(tag['name'], style='filled', color="white")
-        
-        for aliased_tag in alias_data:
-            c.edge(aliased_tag['name'], tag['name'], style='invis')
+        with g.subgraph(name='cluster_{}'.format(tag['name'])) as c:
+            c.attr(style='filled')
 
-    g.edge(tag['name'], current_tag)
+            c.node_attr.update(style='filled', color='lightgrey')
+
+            c.node(tag['name'], style='filled', color="white")
+            
+            for aliased_tag in alias_data:
+                c.edge(aliased_tag['name'], tag['name'], style='invis')
+
+        g.edge(tag['name'], current_tag)
 
 g.view()
